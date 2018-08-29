a3febb21e354f8c73b0f2d702a7c35923bd8d80d
Kyla Palos <kyla.palos@gmail.com>
2018-08-29 19:02:23 +0000

    finished sumItems

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  for(let number of array){
+    if(Array.isArray(number)){
+      total += sumItems(number);
+    } else {
+      total += number;
+    }
+  }
+
+  return total;
+
 }
 
 module.exports = sumItems;
